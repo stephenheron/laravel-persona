c7a6df4969917a7ec806b9b75b6de235784f9c4c
Stephen Heron <stephen.heron@gmail.com>
2013-09-08 12:20:44 +0100

    Fixed indentation issue within service provider

--- src/Heron/Persona/PersonaServiceProvider.php
+++ src/Heron/Persona/PersonaServiceProvider.php
@@ -6,47 +6,47 @@ use browserid\Verifier;
 
 class PersonaServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+  /**
+   * Indicates if loading of the provider is deferred.
+   *
+   * @var bool
+   */
+  protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
+  /**
+   * Bootstrap the application events.
+   *
+   * @return void
+   */
+  public function boot()
+  {
     $this->package('heron/persona');
     include __DIR__.'/../../routes.php';
     include __DIR__.'/../../listeners.php';
-	}
+  }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function register()
   {
     $this->app['persona'] = $this->app->share(function($app) {
       $baseUrl = URL::to('/');
       $verifier = new Verifier($baseUrl);
       return new Persona($verifier);
     });
-	}
+  }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('persona');
-	}
+  /**
+   * Get the services provided by the provider.
+   *
+   * @return array
+   */
+  public function provides()
+  {
+    return array('persona');
+  }
 
 }
